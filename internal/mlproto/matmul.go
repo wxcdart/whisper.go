881976c505432856dba0473c29fc59d71b389ebb
DMEngDev <walter.chang.th@dartmouth.edu>
2026-03-22 12:59:36 +0000

    refactor: standardize code formatting and indentation in matrix multiplication and tensor test files

--- internal/mlproto/matmul.go
+++ internal/mlproto/matmul.go
@@ -5,23 +5,23 @@ import "fmt"
 // MatMul computes C = A * B using a simple triple-loop algorithm.
 // A: m x n, B: n x p -> C: m x p
 func MatMul(a, b *Tensor) (*Tensor, error) {
-    if a.Cols() != b.Rows() {
-        return nil, fmt.Errorf("matmul: incompatible dims %dx%d * %dx%d", a.Rows(), a.Cols(), b.Rows(), b.Cols())
-    }
-    m := a.Rows()
-    n := a.Cols()
-    p := b.Cols()
-    c := NewTensor(m, p)
+	if a.Cols() != b.Rows() {
+		return nil, fmt.Errorf("matmul: incompatible dims %dx%d * %dx%d", a.Rows(), a.Cols(), b.Rows(), b.Cols())
+	}
+	m := a.Rows()
+	n := a.Cols()
+	p := b.Cols()
+	c := NewTensor(m, p)
 
-    for i := 0; i < m; i++ {
-        for k := 0; k < n; k++ {
-            aik := a.At(i, k)
-            baseC := i * p
-            for j := 0; j < p; j++ {
-                c.Data[baseC+j] += aik * b.At(k, j)
-            }
-        }
-    }
+	for i := 0; i < m; i++ {
+		for k := 0; k < n; k++ {
+			aik := a.At(i, k)
+			baseC := i * p
+			for j := 0; j < p; j++ {
+				c.Data[baseC+j] += aik * b.At(k, j)
+			}
+		}
+	}
 
-    return c, nil
+	return c, nil
 }
